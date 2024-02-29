53e010e4ff09ce3315ab08ab664d7a6a1e82c82b
ilqareskerov <ilqar.eskerov@mail.ru>
2024-02-29 23:10:00 +0400

    ready

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
-"""Exercise to practice interview"""
-from itertools import dropwhile
+
+"""First Algorithm Problem"""
 
 
 def canUnlockAll(boxes):
-    """Checks if all boxes can be unlocked"""
+    unlockedBoxes = set([0])
+    leftToExplore = [0]
 
-    keys = {0}
-    range_boxes = range(len(boxes))
+    while leftToExplore:
+        actualBoxeIndex = leftToExplore.pop()
+        keys = boxes[actualBoxeIndex]
 
-    while True:
-        not_found = set()
-
-        for i in range_boxes:
-            if i in keys:
-                for key in dropwhile(lambda k: k in keys, boxes[i]):
-                    keys.add(key)
-            else:
-                not_found.add(i)
-
-        if range_boxes == not_found:
-            return False
-
-        if not not_found:
-            return True
-
-        range_boxes = not_found
+        for key in keys:
+            if key < len(boxes) and key not in unlockedBoxes:
+                unlockedBoxes.add(key)
+                leftToExplore.append(key)
+    return len(unlockedBoxes) == len(boxes)
